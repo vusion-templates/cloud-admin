cc91d2650e7116c145b3f31a227307cefea6a952
zhangsanshi <zhangsanshi5566@gmail.com>
2020-05-09 19:02:40 +0800

    feat: add hash maonaco editor

--- webpack/editor.js
+++ webpack/editor.js
@@ -3,6 +3,7 @@ module.exports = {
         config.plugin('monaco-editor').use(require('monaco-editor-webpack-plugin'), [{
             languages: ['javascript', 'json', 'markdown', 'typescript'],
             publicPath: vueConfig.publicPath, // fix for cross domain
+            filename: 'public/js/[name].[hash:8].worker.js',
         }]);
     },
 };
