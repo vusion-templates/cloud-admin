44060c3e7bb4418794efab8364a156ee743ab027
zhangsanshi <circleci@18419c9443db>
2020-06-10 09:46:55 +0000

    deploy [ci skip]

---
+++ public/js/demo.cloud-admin-latest.js
@@ -0,0 +1,5 @@
+;(function(){
+            var m=window.micro=window.micro||{};
+            var c=m.subApps=m.subApps||{};
+            c["demo.cloud-admin"]={"css":["https://vusion-templates.github.io/cloud-admin/public/css/chunk-vendors.4e644fea.css","https://vusion-templates.github.io/cloud-admin/public/css/dashboard.3745333b.css"],"js":["https://vusion-templates.github.io/cloud-admin/public/js/micro.d744a1b9.js","https://vusion-templates.github.io/cloud-admin/public/js/chunk-vendors.99f057ca.js","https://vusion-templates.github.io/cloud-admin/public/js/dashboard.fefab832.js"]};
+            m.subApps=c;})();
